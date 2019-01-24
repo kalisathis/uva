e6379bb5232b12fdaa1cbdd0fc07b99211f47106
kalisathis <36320295+kalisathis@users.noreply.github.com>
2019-01-24 21:09:13 +0530

    Create uva 573 snail.java

---
+++ uva 573 snail.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+	
+	public static void main (String [] abc) throws IOException  {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		while ((s=br.readLine())!=null) {
+			StringTokenizer st=new StringTokenizer(s);
+			double targetH=Double.parseDouble(st.nextToken());
+			if (targetH==0) {
+				break;
+			}
+			double U=Double.parseDouble(st.nextToken());
+			double D=Double.parseDouble(st.nextToken());
+			double F=Math.max(0.0,U*(Double.parseDouble(st.nextToken())/100.0)); //Ignore F if negative.
+			double currH=0.0;
+			
+			int day=0;
+			while (currH<=targetH && currH>=0.0) {
+				day++;
+				currH=currH+U;
+				if (currH>targetH) {
+					break;
+				}
+				currH=currH-D;
+				U=Math.max(0, U-F);
+			}
+			
+			if (currH>targetH) {
+				System.out.println("success on day "+(day));
+			} else {
+				System.out.println("failure on day "+day);
+			}
+		}
+	}
+}
